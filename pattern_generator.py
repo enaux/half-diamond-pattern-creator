9bc0b6272e41b7bd9ed48f04cb3aa7c2475cfa8d
enaux <enaughton7@protonmail.com>
2024-03-24 18:20:47 +0000

    Updated documentation in pattern_generator.py.

--- pattern_generator.py
+++ pattern_generator.py
@@ -1,6 +1,6 @@
-
+# -------------------------- Half-Diamond Pattern Generator -----------------------------
 """
-This program generates the following pattern:
+This program generates half-diamond patterns as seen below:
 
 *
 **
@@ -12,110 +12,130 @@ This program generates the following pattern:
 **
 *
 
-It then allows the user to generate as many patterns as they wish, and to choose the size.
+Upon startup, the program generates an example of the above pattern for the user to see.
+The program then allows the user to generate as many subsequent patterns as they wish,
+and allows the user to choose the size of each pattern they generate.
 
 """
 
+
+# Display a welcome message for the user.
 print("-"*65)
 print("Welcome to the Half-Diamond Pattern Generator!")
 print("-"*65)
+
+# Display an explanation of the purpose of the program for the user.
 print("The program can generate patterns in the shape of a half-diamond.")
 print("An example is seen below:")
 
 
 
+""" --------------- Display an example of a pattern for the user. ------------------- """
 
-# Initial pattern is printed for the user.
-# Additional print() statements are for spacing.
 star = "*"
 
+# Print line for spacing - to improve readability.
 print()
 
+# Generate and display a half-diamond pattern using the logic within the for-loop.
 for i in range(0,9):
     if i<5:
         print(star*(i+1))
     else:
         print(star*(9-i))
 
+# Print line for spacing - to improve readability.
 print()
 
 
+""" -------------- Allow user to generate another pattern or to exit. --------------- """
 
-
-# Then allow user to choose to generate another pattern or to exit.
-        
-
+# Set done variable to False - this will control user exit.
 done = False
 
-# Ask the user to choose either to continue or to exit.
+# Display a divider for readability.
 print("-"*50)
+
+# Ask the user if they wish to continue or to exit.
 print("Would you like to create another pattern?")
+# Request input of an answer from the user.
 user_choice = input("Enter 'yes' or 'no':\t")
+# Convert to lowercase before input validation.
 user_choice = user_choice.lower()
 
-# while-loop to check if user entry is valid.
+
+# Perform input validation using a while-loop.
 while (user_choice != "yes"):
+
     if (user_choice == "no"):
-        # If user chooses no, 'done' is set to True and the below while-loop is never entered.
-        # i.e. no more patterns are created.
+        # Set done to True, to allow exit from the program.
         done = True
+        # Break the while-loop and return to the main program.
         break
+
+    # If the user enters anything other than "yes" or "no":
     else:
-        print()
-        print("*** Invalid Entry ***")
-        print()
+        # Display an error message for the user.
+        print("\n*** Invalid Entry ***\n")
+        # Request input of another answer from the user.
         user_choice = input("Please enter 'yes' or 'no':\t")
+        # Convert new input to lowercase before input validation.
         user_choice = user_choice.lower()
 
 
-# while-loop will generate patterns for the user as long as they choose to continue.
+# The below while-loop will generate patterns as long as the user chooses to continue.
 while not done:
 
-    # Allow the user to choose the pattern size.
-    print()
-    print("How long would you like the pattern to be?")
+    # Request input from the user of the pattern length.
+    print("\nHow long would you like the pattern to be?")
     pattern_length = int(input("Please enter an odd number:\t"))
 
     # Ensure the user chooses an odd number to ensure a pointy half-diamond shaped pattern.
     while ((pattern_length % 2) != 1):
-        print()
-        print("The program needs an odd number to give it a nice pattern!")
-        print()
+        print("\nThe program needs an odd number to give it a nice pattern!\n")
         pattern_length = int(input("Please enter an odd number:\t"))
 
+    # Display a divider for readability.
     print("-"*50)
     print()
 
-    # Print pattern according to the length chosen by the user.
+    # Generate and display a half-diamond pattern using the length chosen by the user.
     for i in range(0,pattern_length):
         if i<(pattern_length/2):
             print(star*(i+1))
         else:
             print(star*(pattern_length-i))
 
-    # Offer the user another choice, to continue or exit.
+    # Display a divider for readability.
     print()
     print("-"*50)
+
+    # Ask the user if they wish to generate another pattern or to exit the program.
     print("Would you like to create another pattern?")
+    # Request input of an answer from the user.
     user_choice = input("Enter 'yes' or 'no':\t")
+    # Convert to lowercase prior to input validation.
     user_choice = user_choice.lower()
 
-    # while-loop to check if user entry is valid.
+    # Perform input validation using a while-loop.
     while (user_choice != "yes"):
+
         if (user_choice == "no"):
-            # If user chooses no, 'done' is set to True to exit the main loop.
+            # Set done to True.
             done = True
+            # Break the while-loop to proceed to exit from the main program.
             break
+
+        # If the user enters anything other than "yes" or "no":
         else:
-            print()
-            print("*** Invalid Entry ***")
-            print()
+            # Display an error message for the user.
+            print("\n*** Invalid Entry ***\n")
+            # Request input of another answer from the user.
             user_choice = input("Please enter 'yes' or 'no':\t")
+            # Convert to lowercase prior to input validation.
             user_choice = user_choice.lower()
 
 
-# Exit message.
+# Display an exit message for the user.
 print("-"*50)
-print()
-print("You have chosen to exit the program. Goodbye.")
-print()
+print("\nYou have chosen to exit the program. Goodbye.\n")
